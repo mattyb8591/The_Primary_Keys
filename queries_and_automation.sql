c6e6219f8e24235377064a4d1cef213439fc392b
Sarah Jabian <142950731+sxrah404@users.noreply.github.com>
2025-05-01 13:13:08 -0500

    Update queries_and_automation.sql

--- queries_and_automation.sql
+++ queries_and_automation.sql
@@ -9,6 +9,18 @@ GROUP BY e.event_id, e.name
 ORDER BY tickets_sold DESC;
 
 -- A query with a subquery:
+/* This query retrieves event names that have not sold any tickets. The subquery finds events with at least one ticket sold by joining 
+the Sessions and Tickets tables, and then the outer query filters out those events, and returns the ones with no ticket sales.
+It returns nothing if all events have sold tickets. */
+SELECT e.name AS event_name
+FROM events e
+WHERE e.event_id NOT IN (
+    SELECT s.event_id
+    FROM sessions s
+    JOIN tickets t ON t.session_id = s.session_id
+    GROUP BY s.event_id
+    HAVING COUNT(t.ticket_id) > 0
+);
 
 -- A query to create a view and another query to demonstrate its use:
 
